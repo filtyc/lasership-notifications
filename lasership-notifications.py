e733773081a8684a81a60196327416fbe058cd77
Filip Leski <filtyc@gmail.com>
2018-03-19 21:00:56 -0400

    Fix a typo

--- lasership-notifications.py
+++ lasership-notifications.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 message = """From: Lasership Notifications
-Subject: Laserhip is here.
+Subject: Lasership is here.
 """
 print("Enter tracking number:", end = " ", flush=True)
 trackingNum = input()
@@ -18,22 +18,22 @@ senderPass = getpass.getpass("Sender's gmail password: ")
 print("Receiver's gmail username:", end = " ", flush=True)
 receiverUser = input()
 
-while True:
-    try:
-        lsurl = urllib.request.urlopen(pkgURL)
-        data = json.loads(lsurl.read().decode("utf-8"))
-        if (data["Events"][0]["EventLabel"] == "Delivered"):
-            smtpObj = smtplib.SMTP('smtp.gmail.com:587')
-            smtpObj.starttls()
-            smtpObj.login(senderUser, senderPass)
-            smtpObj.sendmail(senderUser + "@gmail.com",
-                receiverUser + "@gmail.com", message)
-            break
-        else:
-            print(datetime.now().strftime('%I:%M%p')
-                + " - Lasership is not here yet.")
-            sleep(60)
-    except:
-        print(datetime.now().strftime('%I:%M%p')
-            + " - I can't check if Lasership is here right now.")
-        sleep(60)
+# while True:
+#     try:
+#         lsurl = urllib.request.urlopen(pkgURL)
+#         data = json.loads(lsurl.read().decode("utf-8"))
+#         if (data["Events"][0]["EventLabel"] == "Delivered"):
+smtpObj = smtplib.SMTP('smtp.gmail.com:587')
+smtpObj.starttls()
+smtpObj.login(senderUser, senderPass)
+smtpObj.sendmail(senderUser + "@gmail.com",
+    receiverUser + "@gmail.com", message)
+    #         break
+    #     else:
+    #         print(datetime.now().strftime('%I:%M%p')
+    #             + " - Lasership is not here yet.")
+    #         sleep(60)
+    # except:
+    #     print(datetime.now().strftime('%I:%M%p')
+    #         + " - I can't check if Lasership is here right now.")
+    #     sleep(60)
